8ae79e1559241c89f1ad112b71f73603f4df2d47
Akshay Singh <8209379+akshayKrSingh@users.noreply.github.com>
2020-08-22 21:58:46 +0530

    feat(configuration): switching to https

--- alcore.mk
+++ alcore.mk
@@ -1,7 +1,14 @@
 service.config.lms.get:
 	docker cp edx.devstack-juniper.master.lms:/edx/etc/lms.yml ./alcore/lms.yml
 
+service.config.studio.get:
+	docker cp edx.devstack-juniper.master.studio:/edx/etc/studio.yml ./alcore/studio.yml
+
 service.config.lms.put:
 	docker cp ./alcore/lms.yml edx.devstack-juniper.master.lms:/edx/etc/lms.yml
 
+service.config.studio.put:
+	docker cp ./alcore/studio.yml edx.devstack-juniper.master.studio:/edx/etc/studio.yml
+
 dev.rebuild.lms: service.config.lms.put dev.static.lms dev.restart-devserver.lms
+dev.rebuild.studio: service.config.studio.put dev.static.studio dev.restart-devserver.studio
